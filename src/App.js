d3fcc78e411119a09b464daa44e393e8544050b6
jakdev047 <jakfreelancer047@gmail.com>
2020-09-17 17:33:32 +0600

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// sass
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/scss/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello world</h2>
+    </div>
+  );
+}
+
+export default App;
